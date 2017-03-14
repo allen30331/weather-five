491aaad5d6f2ad90d3352c3aef6169ed238db0f9
allen30331 <allen30331@gmail.com>
2017-03-14 11:10:55 -0400

    added https to request

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY= 'ea2543f66cae3ed4a4d636a227b2570b';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
