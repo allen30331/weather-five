d0857c5ad118abbf10473fce0537f4fc63067e59
allen30331 <allen30331@gmail.com>
2017-03-13 21:58:40 -0400

    firs push

---
+++ js/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Test() {
+	return (
+		<div>Please help me</div>
+		);
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './components/app';
+//import reducers from './reducers';
 
+
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<Test />, document.getElementById('container'))
+);
