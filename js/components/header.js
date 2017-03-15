150cf20923f5b3e89cdb93a2bc8a56997aba192a
allen30331 <allen30331@gmail.com>
2017-03-15 11:00:27 -0400

    changed media query

--- js/components/header.js
+++ js/components/header.js
@@ -4,8 +4,8 @@ export default function header() {
 	return (
 		<div className="jumbotron">
 			<div className="container">
-				<h1>Weather 5</h1>
-				<h4>Five day forecasts</h4>
+				<h1 className="weather">Weather 5</h1>
+				<h4 className="five">Five day forecasts</h4>
 			</div>
 		</div>
 	)
