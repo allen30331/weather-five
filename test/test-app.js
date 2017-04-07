886b8af616466c6699ffeee828e224b2d5a71d6e
allen30331 <allen30331@gmail.com>
2017-04-07 10:51:18 -0400

    combined tests into one file

--- test/test-app.js
+++ test/test-app.js
@@ -2,11 +2,14 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
 import App from '../js/components/app';
+import Categories from '../js/components/categories';
+import Header from '../js/components/header';
+
 
 const should = chai.should();
 
 
-describe('App component', function() {
+describe('Tests Components', function() {
 
 	it('should render other components and containers', function() {
 		const renderer = TestUtils.createRenderer();
@@ -17,5 +20,28 @@ describe('App component', function() {
 
 		result.type.should.equal('div');
 		result.props.className.should.equal('app-container');
+	});
+
+	
+	it('should render the header that displays the weather categories', function() {
+		const renderer = TestUtils.createRenderer();
+
+		renderer.render(<Categories />);
+
+		const result = renderer.getRenderOutput();
+
+		result.type.should.equal('div');
+		result.props.className.should.equal('row categories');
+	});
+
+	it('should render the heading', function() {
+		const renderer = TestUtils.createRenderer();
+
+		renderer.render(<Header />);
+
+		const result = renderer.getRenderOutput();
+
+		result.type.should.equal('div');
+		result.props.className.should.equal('header-container');
 	});
 });
